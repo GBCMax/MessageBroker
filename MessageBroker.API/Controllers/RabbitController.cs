b236b87676939919ba52ae83c3c874d6bc756f6d
GBCMax <maks_on_max@mail.ru>
2024-05-12 15:47:39 +0700

    Добавьте файлы проекта.

---
+++ MessageBroker.API/Controllers/RabbitController.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using RabbitMQ.Client;
+using System.Text;
+using System.Text.Json;
+
+namespace MessageBroker.API.Controllers
+{
+    [ApiController]
+    [Route("Rabbit")]
+    public class RabbitController : ControllerBase
+    {
+        [HttpPost("Send")]
+        public IActionResult Send(object message)
+        {
+            var factory = new ConnectionFactory { HostName = "localhost" };
+            using (var connection = factory.CreateConnection())
+            {
+                using (var channel = connection.CreateModel())
+                {
+                    channel.QueueDeclare(queue: "MyQueue",
+                        durable: false,
+                        exclusive: false,
+                        autoDelete: false,
+                        arguments: null);
+
+                    var body = Encoding.UTF8.GetBytes(JsonSerializer.Serialize(message));
+
+                    channel.BasicPublish(exchange: "",
+                        routingKey: "MyQueue",
+                        basicProperties: null,
+                        body: body);
+                }
+            }
+            return Ok(message);
+        }
+    }
+}
---
+++ MessageBroker.API/RabbitBgWorker.cs
@@ -0,0 +1,42 @@
+﻿
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System.Text;
+
+namespace MessageBroker.API
+{
+    public class RabbitBgWorker : BackgroundService
+    {
+        private IConnection _connection;
+        private IModel _channel;
+        public RabbitBgWorker()
+        {
+            var factory = new ConnectionFactory { HostName = "localhost" };
+            _connection = factory.CreateConnection();
+            _channel = _connection.CreateModel();
+            _channel.QueueDeclare(queue: "MyQueue", 
+                durable: false, 
+                exclusive: false, 
+                autoDelete: false, 
+                arguments: null);
+        }
+        protected override Task ExecuteAsync(CancellationToken stoppingToken)
+        {
+            stoppingToken.ThrowIfCancellationRequested();
+
+            var consumer = new EventingBasicConsumer(_channel);
+            consumer.Received += Consume;
+
+            _channel.BasicConsume("MyQueue", false, consumer);
+
+            return Task.CompletedTask;
+        }
+
+        private void Consume(object? ch, BasicDeliverEventArgs eventArgs)
+        {
+            var content = Encoding.UTF8.GetString(eventArgs.Body.ToArray());
+
+            _channel.BasicAck(eventArgs.DeliveryTag, false);
+        }
+    }
+}
